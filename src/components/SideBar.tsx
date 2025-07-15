c933c9d70f93d4fcfbf97177ee2cd7921c182491
sean <j.sean1316@gmail.com>
2025-07-15 18:21:57 +0900

    FIX[sideBar]: css 수정(모바일)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -24,8 +24,8 @@ export default function SideBar() {
   return (
     <div className="w-1/4 h-full bg-[#2B2D32] border-r-2 border-gray-500 p-4">
       <div className="flex flex-col gap-4">
-        <p className="text-xl font-bold">æ-todo</p>
-        <div className="flex flex-col gap-2 text-sm ">
+        <p className="text-sm sm:text-base font-bold">æ-todo</p>
+        <div className="flex flex-col gap-2 text-[12px] sm:text-sm">
           {routes.map((route) => {
             const { path, label } = route;
             const isActive = router === path;
