785332dd37eee81fa0b60abbda9d0ef157b93986
sean <j.sean1316@gmail.com>
2025-07-15 17:09:38 +0900

    FEAT[sideBar]: layout단으로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import QueryProvider from "@/providers/QueryProvider";
+import SideBar from "@/components/SideBar";
 
 export const metadata: Metadata = {
   title: "ae-todo",
@@ -14,8 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        <QueryProvider>{children}</QueryProvider>
+      <body className="antialiased flex h-screen w-full">
+        <QueryProvider>
+          <div className="flex h-screen w-full gap-8">
+            <SideBar />
+            {children}
+          </div>
+        </QueryProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,16 +74,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-screen gap-8">
-      <div className="w-1/4 h-full bg-[#2B2D32] border-r-2 border-gray-500 p-4">
-        <div className="flex flex-col gap-4">
-          <p className="text-xl font-bold">æ-todo</p>
-          <div className="flex flex-col gap-2">
-            <p>오늘 할 일</p>
-            <p>내일 할 일</p>
-          </div>
-        </div>
-      </div>
+    <div className="flex h-screen w-full">
       <main className="flex flex-col gap-[12px] row-start-2 items-start w-full max-w-md py-14 px-4 ">
         <p className="text-xl font-bold">해낸다.</p>
         <div className="flex flex-col gap-[4px] w-full">
